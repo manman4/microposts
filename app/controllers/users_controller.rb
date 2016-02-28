c10b93d330133138abfbd2092b3ad935181d83f4
manman4 <manchanr4@gmail.com>
2016-02-28 04:47:05 +0000

    0228

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to 'show' , notice: 'メッセージを編集しました'
+      redirect_to @user , notice: 'メッセージを編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :area, presence: true, length: { maximum: 100 }
+  validates :hobby, presence: true, length: { maximum: 100 }
   has_secure_password
 end
